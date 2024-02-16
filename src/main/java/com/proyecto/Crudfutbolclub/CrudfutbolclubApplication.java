d3ebd08dd7e95c6216f6a902a0b8b76171f05368
Brayan Yesid Mejia Ortega <elyesid22@gmail.com>
2024-02-16 15:46:24 -0500

    Second commit

---
+++ src/main/java/com/proyecto/Crudfutbolclub/CrudfutbolclubApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.Crudfutbolclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfutbolclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfutbolclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/Crudfutbolclub/CrudfutbolclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.Crudfutbolclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfutbolclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
